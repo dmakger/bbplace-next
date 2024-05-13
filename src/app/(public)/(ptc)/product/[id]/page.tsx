c40030af45edf92631b2813efcb8dd5b2fce7851
dmakger <dmakger@yandex.ru>
2024-05-13 20:06:15 +0300

    fix

--- src/app/(public)/(ptc)/product/[id]/page.tsx
+++ src/app/(public)/(ptc)/product/[id]/page.tsx
@@ -2,19 +2,14 @@
 
 import { CurrencyAPI } from "@/entities/Metrics/api/currency.metrics.api";
 import { MetricsAPI } from "@/entities/Metrics/api/metrics.metrics.api";
-import { getDiapason } from "@/entities/Metrics/lib/metrics/diapason.metrics.metrics.lib";
-import { IWholesale } from "@/entities/Metrics/model/wholesale.metrics.model";
 import { ProductAPI } from "@/entities/Product/api/product.api";
 import { productApiToProduct } from "@/entities/Product/lib/product.lib";
 import { IProduct } from "@/entities/Product/model/product.model";
-import { PriceQuantity } from "@/shared/ui/PriceQuantity/PriceQuantity";
 import Wrapper1280 from "@/shared/ui/Wrapper/1280/Wrapper1280";
 import { CatalogImage } from "@/widgets/CatalogImage/CatalogImage";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import cl from './_ProductDetailPage.module.scss'
-import { HeadingToText } from "@/shared/ui/Text";
-import { getDate } from "@/shared/lib/dateTime.lib";
 import { WrapperBlock } from "@/shared/ui/Wrapper/Block/WrapperBlock";
 import { BlockInfoProduct } from "@/features/Block/Info/Product/BlockInfoProduct";
 
