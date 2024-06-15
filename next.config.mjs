df7b91342a49032faf2c6ce3d635904738ee9765
dmakger <dmakger@yandex.ru>
2024-06-15 19:18:06 +0300

    add config in next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      // This is experimental but can
+      // be enabled to allow parallel threads
+      // with nextjs automatic static generation
+      workerThreads: false,
+      cpus: 1
+    },
+  };
+  
+  export default nextConfig;
+  
