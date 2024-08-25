a54e876d3fb399f5d63cde4471fa4c384d7c3ba9
dmakger <dmakger@yandex.ru>
2024-08-25 17:02:15 +0300

    Fix header search

--- src/shared/data/ptc.data.ts
+++ src/shared/data/ptc.data.ts
@@ -1,8 +1,8 @@
 import { IOption } from "@/shared/model/option.model";
 
-export const PRODUCT_PTC_ITEM_DATA: IOption = {id: 1, name: "Товары", value: 'product' }
-export const TENDER_PTC_ITEM_DATA: IOption = {id: 2, name: "Тендеры", value: 'tender'}
-export const COMPANY_PTC_ITEM_DATA: IOption = {id: 3, name: "Компании", value: 'supplier'}
+export const PRODUCT_PTC_ITEM_DATA: IOption = {id: 1, name: "Товары", value: 'products' }
+export const TENDER_PTC_ITEM_DATA: IOption = {id: 2, name: "Тендеры", value: 'tenders'}
+export const COMPANY_PTC_ITEM_DATA: IOption = {id: 3, name: "Поставщики", value: 'suppliers'}
 
 export const PTC_LIST_DATA: IOption[] = [
     PRODUCT_PTC_ITEM_DATA,
