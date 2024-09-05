24efda6a02a0910e2cb843cd530597c469703c64
Ilya Yudenkov <yupii2051@gmail.com>
2024-09-05 16:57:16 +0300

    add additionalBlockButtons at CheckEmailChildrenPage, add social icons of yandex, vk, google, add new colors

---
+++ src/shared/ui/Icon/data/google.data.icon.ts
@@ -0,0 +1,9 @@
+import { IIcon } from '../model/icon.model'
+import GoogleSecondaryIcon from '@/shared/assets/img/Google/GoogleSecondaryIcon.svg'
+import GoogleDefaultIcon from '@/shared/assets/img/Google/GoogleDefaultIcon.svg'
+
+export const GOOGLE_ICON: IIcon = {
+    default: GoogleSecondaryIcon,
+    defaultHovered: GoogleDefaultIcon,
+    defaultPressed: GoogleDefaultIcon
+}
--- src/widgets/Pages/LogIn/CheckEmailChildrenPage/ui/CheckEmailChildrenPage.tsx
+++ src/widgets/Pages/LogIn/CheckEmailChildrenPage/ui/CheckEmailChildrenPage.tsx
@@ -1,19 +1,26 @@
 'use client'
 
+import cl from './_CheckEmailChildrenPage.module.scss'
 import { WrapperForLogInNSupportPages } from "@/shared/ui/Wrapper/ForLogInNSupportPages"
 import { WrapperRectangleInput } from "@/shared/ui/Wrapper/RectangleInput"
 import Input from "@/shared/ui/Input/Input"
 import { EInputVariants } from "@/shared/ui/Input/model/input.model"
 import { UserAPI } from "@/entities/Auth/api/auth.api"
-import { FormEvent, useRef, useState } from "react"
+import { FormEvent, ReactNode, useRef, useState } from "react"
 import { getFormDataFromForm } from "@/shared/lib/formData.lib"
 import { useRouter } from "next/navigation"
 import { MAIN_PAGES } from "@/config/pages-url.config"
 import { useActionCreators } from "@/storage/hooks"
 import { EMAIL_VALID_RULES, isEmailValid } from "@/entities/Auth/data/telNEmail.data"
 import { FILL_THE_FIELD } from "@/entities/Auth/data/errorMessages.data"
-import { ButtonType } from "@/shared/ui/Button/model/button.model"
+import { ButtonColor, ButtonSize, ButtonType, ButtonVariant } from "@/shared/ui/Button/model/button.model"
 import { EInputTextType } from "@/shared/ui/Input/ui/Text/data/text.input.data"
+import { Button } from "@/shared/ui/Button"
+import { VK_SECONDARY_ICON } from "@/shared/ui/Icon/data/vk.data"
+import { YANDEX_ICON } from "@/shared/ui/Icon/data/yandex.data.icon"
+import { GOOGLE_ICON } from "@/shared/ui/Icon/data/google.data.icon"
+import { cls } from '@/shared/lib/classes.lib'
+
 
 export const CheckEmailChildrenPage = () => {
 
@@ -68,8 +75,21 @@ export const CheckEmailChildrenPage = () => {
         }
     }
 
+    //VARIABLE
+    const additionalBlockButtons: ReactNode[] = [
+        <Button className={cls(cl.socialButton, cl.vk)} title='ВКонтакте' variant={ButtonVariant.TONAL} color={ButtonColor.Secondary} size={ButtonSize.Medium} beforeImage={VK_SECONDARY_ICON}/>,
+        <Button className={cls(cl.socialButton, cl.yandex)} title='Яндекс' variant={ButtonVariant.TONAL} color={ButtonColor.Secondary}  size={ButtonSize.Medium} beforeImage={YANDEX_ICON} />,
+        <Button className={cls(cl.socialButton, cl.google)} title='Google' variant={ButtonVariant.TONAL} color={ButtonColor.Secondary} size={ButtonSize.Medium} beforeImage={GOOGLE_ICON}/>
+    ]
+
     return (
-        <WrapperForLogInNSupportPages pageTitle="Вход или регистрация профиля" onSubmitFunc={CheckEmail} formRef={formRef}>
+        <WrapperForLogInNSupportPages
+            pageTitle="Вход или регистрация профиля"
+            onSubmitFunc={CheckEmail}
+            formRef={formRef}
+            additionalBlockTitle="Через сторонние сервисы:"
+            additionalBlockButtons={additionalBlockButtons}
+        >
             <WrapperRectangleInput
                 labelText="Электронная почта"
                 isRequired
