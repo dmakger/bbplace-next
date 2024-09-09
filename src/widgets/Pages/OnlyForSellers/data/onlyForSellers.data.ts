bf2c3ef14fea9a8590ac3e7301fdc535d55a1136
Ilya Yudenkov <yupii2051@gmail.com>
2024-09-09 10:57:34 +0300

    fix socialButton color, WrapperForLoginNSupportPages gap at 768px, additionalBlockButton overflow at 768px

--- src/widgets/Pages/OnlyForSellers/data/onlyForSellers.data.ts
+++ src/widgets/Pages/OnlyForSellers/data/onlyForSellers.data.ts
@@ -1,5 +1,6 @@
 import { DASHBOARD_PAGES } from "@/config/pages-url.config";
 
 export const ONLY_FOR_SELLERS_PAGES_ARRAY: string[] = [
-    DASHBOARD_PAGES.PRODUCTS.path
+    DASHBOARD_PAGES.PRODUCTS.path,
+    DASHBOARD_PAGES.PRICES_N_DISCOUNTS.path
 ]
