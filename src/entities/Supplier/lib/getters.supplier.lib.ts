4b31b370801db2ed841336aa444026bce9db6344
Герасимов Дмитрий <dmakger@yandex.ru>
2024-10-07 15:20:10 +0300

    Fix getNameSupplier

--- src/entities/Supplier/lib/getters.supplier.lib.ts
+++ src/entities/Supplier/lib/getters.supplier.lib.ts
@@ -1,10 +1,20 @@
 import { ICategory } from "@/entities/Metrics/model/category.metrics.model";
 import { ISupplier } from "../model/supplier.model";
 
+/**
+ * Возращает имя поставщика
+ */
 export const getNameSupplier = (supplier: ISupplier) => {
-    if (supplier.brandName.trim())
-        return supplier.brandName
-    return supplier.legalName
+    console.log('qew supplier', supplier)
+    // const {brandName, legalName, fullName, } = supplier
+    const brandName = supplier.brandName.trim()
+    if (brandName)
+        return brandName
+
+    const legalName = supplier.legalName?.trim()
+    if (legalName)
+        return legalName
+    return supplier.fullName.trim()
 }
 
 export const getCategoryNameSupplier = (category: ICategory[]) => {
