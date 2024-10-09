de8d4ede1b5b14084a59148909d27a3652097b1f
Герасимов Дмитрий <dmakger@yandex.ru>
2024-10-09 17:49:25 +0300

    Fix error build

--- src/shared/ui/Wrapper/NoEmail/ui/WrapperNoEmail.tsx
+++ src/shared/ui/Wrapper/NoEmail/ui/WrapperNoEmail.tsx
@@ -4,7 +4,8 @@ import { MAIN_PAGES } from "@/config/pages-url.config";
 import { Loader } from "@/shared/ui/Loader";
 import { useAppSelector } from "@/storage/hooks";
 import { useRouter, useSearchParams } from "next/navigation";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, SetStateAction, useEffect, useState } from "react";
+import SuspenseL from "../../SuspenseL/SuspenseL";
 
 interface IWrapperNoEmail {
     children: ReactNode;
@@ -18,24 +19,22 @@ interface IWrapperNoEmail {
 export const WrapperNoEmail = ({ children }: IWrapperNoEmail) => {
     // RTK
     const { email } = useAppSelector(state => state.user);
-
-    //SEARCH_PARAMS
-    const emailFromUrl = useSearchParams().get('email') || undefined;
-
+    
     // ROUTER
     const router = useRouter();
 
     //STATE
+    const [emailURL, setEmailURL] = useState<string | undefined>();
     const [isClient, setIsClient] = useState(false);
 
     //EFFECT
     useEffect(() => {
         setIsClient(true);        
       
-        if (!email && !emailFromUrl) {
-            router.push(MAIN_PAGES.CHECK_EMAIL.path); // Перенаправляем, если нет email или emailFromUrl
+        if (!email && !emailURL) {
+            router.push(MAIN_PAGES.CHECK_EMAIL.path); // Перенаправляем, если нет email или emailURL
         }
-    }, [email, emailFromUrl]);
+    }, [email, emailURL]);
 
     // Возвращаем null на сервере, чтобы избежать несоответствия в гидратации
     if (!isClient) {
@@ -43,12 +42,14 @@ export const WrapperNoEmail = ({ children }: IWrapperNoEmail) => {
     }
 
     return (
-        <>
-            {email || emailFromUrl ? (
+        <SuspenseL.Any data={[
+            { searchKey: 'email', set: setEmailURL}
+        ]}>
+            {email || emailURL ? (
                 <>{children}</>
             ) : (
                 <Loader />
             )}
-        </>
+        </SuspenseL.Any>
     );
 };
