13f4b3662dcec248e4be8339c9bf9a5fea6bb9be
dmakger <dmakger@yandex.ru>
2024-10-10 18:55:16 +0300

    test

--- src/widgets/Pages/Test/TestChildrenPage.tsx
+++ src/widgets/Pages/Test/TestChildrenPage.tsx
@@ -9,8 +9,8 @@ export const TestChildrenPage:FC<TestChildrenPageProps> = ({className}) => {
     const startImageURL = "https://bbplace.ru/fileservice/api/FilesS3/GetFile/";
     const imageKey = "040ec9c7-97ad-49e6-ba0a-8764fe5afcc9.JPG";
     // const imageUrl = "https://bbplace.ru/fileservice/api/FilesS3/GetFile/040ec9c7-97ad-49e6-ba0a-8764fe5afcc9.JPG";
-    const imageUrl = "https://hb.bizmrg.com/image_store/040ec9c7-97ad-49e6-ba0a-8764fe5afcc9.JPG";
-    // const imageUrl = "https://hb.bizmrg.com/image_store/ae4e2223-d0ba-4752-9f2a-f378f1275f19.jpg";
+    // const imageUrl = "https://hb.bizmrg.com/image_store/040ec9c7-97ad-49e6-ba0a-8764fe5afcc9.JPG";
+    const imageUrl = "https://hb.bizmrg.com/image_store/ae4e2223-d0ba-4752-9f2a-f378f1275f19.jpg";
 
     return (
         // 2.2 MB
